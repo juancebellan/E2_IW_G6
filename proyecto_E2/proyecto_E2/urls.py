f3c1a0a90c9c9cbea882bfac8ea4123eb0b819e5
Xabierlb <xabierlb22@gmail.com>
2025-04-30 10:24:58 +0200

    cambio en nombres, comienzo a hacer templates

--- proyecto_E2/proyecto_E2/urls.py
+++ proyecto_E2/proyecto_E2/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 urlpatterns = [
- path('admin/', admin.site.urls),
- path('GP/', include('gestion_produccion_G6.urls'))
+    path('', include('gestion_produccion_G6.urls')),
+    path('admin/', admin.site.urls),
+    path('GP/', include('gestion_produccion_G6.urls'))
 ]
 
