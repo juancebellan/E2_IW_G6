a570901a35288dfca8d01c21ee4c667e64a06267
Xabierlb <xabierlb22@gmail.com>
2025-05-05 11:18:13 +0200

    Mejorado uso de context_object_name y redirecciones tras edición para volver a la vista de detalle

--- proyecto_E2/gestion_produccion_G6/views.py
+++ proyecto_E2/gestion_produccion_G6/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Cliente, Empleado, Proyecto, Tarea
-from django.urls import reverse_lazy
+from django.urls import reverse, reverse_lazy
 
 
 def landing(request):
@@ -18,6 +18,7 @@ class ProyectoListView(ListView):
 class ProyectoDetailView(DetailView):
     model = Proyecto
     template_name = 'proyecto_detail.html'
+    context_object_name = 'proyecto'
 
 class ProyectoCreateView(CreateView):
     model = Proyecto
@@ -29,12 +30,15 @@ class ProyectoUpdateView(UpdateView):
     model = Proyecto
     fields = '__all__'
     template_name = 'proyecto_form.html'
-    success_url = reverse_lazy('proyecto_list')
+    context_object_name = 'proyecto'
+    def get_success_url(self):
+        return reverse('proyecto_detail', kwargs={'pk': self.object.pk})
 
 class ProyectoDeleteView(DeleteView):
     model = Proyecto
     template_name = 'proyecto_confirm_delete.html'
     success_url = reverse_lazy('proyecto_list')
+    context_object_name = 'proyecto'
 
 
 # CLIENTE
@@ -53,16 +57,19 @@ class ClienteUpdateView(UpdateView):
     model = Cliente
     fields = '__all__'
     template_name = 'cliente_form.html'
-    success_url = reverse_lazy('cliente_list')
+    context_object_name = 'cliente'
+    def get_success_url(self):
+        return reverse('cliente_detail', kwargs={'pk': self.object.pk})
 
 class ClienteDetailView(DetailView):
     model = Cliente
     template_name = 'cliente_detail.html'
-    context_object_name = "mi_cliente"
+    context_object_name = "cliente"
 
 class ClienteDeleteView(DeleteView):
-    model = Proyecto
+    model = Cliente
     template_name = 'cliente_confirm_delete.html'
+    context_object_name = 'cliente'
     success_url = reverse_lazy('cliente_list')
 
 
@@ -88,19 +95,22 @@ class EmpleadoUpdateView(UpdateView):
     model = Empleado
     fields = '__all__'
     template_name = 'empleado_form.html'
-    success_url = reverse_lazy('empleado_list')
+    context_object_name = 'empleado'
+    def get_success_url(self):
+        return reverse('empleado_detail', kwargs={'pk': self.object.pk})
 
 class EmpleadoDeleteView(DeleteView):
     model = Empleado
     template_name = 'empleado_confirm_delete.html'
     success_url = reverse_lazy('empleado_list')
+    context_object_name = 'empleado'
 
     
 # TAREA
 class TareaListView(ListView):
     model = Tarea
     template_name = 'tarea_list.html'
-    context_object_name = 'mi_tarea'
+    context_object_name = 'tareas'
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['fecha_actual'] = date.today()
@@ -108,24 +118,24 @@ class TareaListView(ListView):
 class TareaDetailView(DetailView):
     model = Tarea
     template_name = 'tarea_detail.html'
-    context_object_name = 'mi_tarea'
+    context_object_name = 'tarea'
 
 class TareaCreateView(CreateView):
     model = Tarea
     fields = '__all__'
     template_name = 'tarea_form.html'
     success_url = reverse_lazy('tarea_list')
-    context_object_name = "tareas"
 
 class TareaUpdateView(UpdateView):
     model = Tarea
     fields = '__all__'
     template_name = 'tarea_form.html'
-    success_url = reverse_lazy('tarea_list')
-    context_object_name = "tareas"
+    context_object_name = "tarea"
+    def get_success_url(self):
+        return reverse('tarea_detail', kwargs={'pk': self.object.pk})
 
 class TareaDeleteView(DeleteView):
     model = Tarea
     template_name = 'tarea_confirm_delete.html'
+    context_object_name = "tarea"
     success_url = reverse_lazy('tarea_list')
-
