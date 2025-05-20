387c9b50b35063c91488d3285ee0da566b0a063f
Unai Pinilla <unai.pinilla@opendeusto.es>
2025-05-20 21:02:14 +0200

    Mejoras de manejo y vistas, e inserccion de registros en bd

--- proyecto_E2/gestion_produccion_G6/views.py
+++ proyecto_E2/gestion_produccion_G6/views.py
@@ -34,6 +34,7 @@ class ProyectoListView(ListView):
     model = Proyecto
     template_name = 'proyecto_list.html'
     context_object_name = 'proyectos'
+
     def get(self, request, *args, **kwargs):
         vengo_de = request.GET.get('vengo_de', None)
         if vengo_de == "boton_arriba":
@@ -41,21 +42,18 @@ class ProyectoListView(ListView):
         return super().get(request, *args, **kwargs)
 
 
-
 class ProyectoDetailView(DetailView):
     """Vista que muestra el detalle de un proyecto específico."""
     model = Proyecto
     template_name = 'proyecto_detail.html'
     context_object_name = 'proyecto'
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['proyecto_cliente'] = self.object.Clientes.all()
+        context['proyecto_cliente'] = self.object.cliente
         context = if_detail(self.request, context)
         context['origen']=self.request.session.get('origen',None)
         return context
-
-       
-
 
 
 class ProyectoCreateView(CreateView):
@@ -64,6 +62,7 @@ class ProyectoCreateView(CreateView):
     form_class = ProyectoForm  
     template_name = 'proyecto_form.html'
     success_url = reverse_lazy('proyecto_list')
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context = if_detail(self.request, context)
@@ -90,13 +89,12 @@ class ProyectoDeleteView(DeleteView):
     template_name = 'proyecto_confirm_delete.html'
     success_url = reverse_lazy('proyecto_list')
     context_object_name = 'proyecto'
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context = if_detail(self.request,context)
         return context
-   
-        
-        
+
 
 
 # CLIENTE
@@ -106,17 +104,20 @@ class ClienteListView(ListView):
     template_name = 'cliente_list.html'
     context_object_name = 'clientes'
     ordering = ['nombre']
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context = if_detail(self.request, context)
         context['origen']=self.request.session.get('origen',None)
         return context
 
+
 class ClienteDetailView(DetailView):
     """Vista que muestra el detalle de un cliente específico."""
     model = Cliente
     template_name = 'cliente_detail.html'
     context_object_name = "cliente"
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context = if_detail(self.request, context)
@@ -131,6 +132,7 @@ class ClienteCreateView(CreateView):
     template_name = 'cliente_form.html'
     success_url = reverse_lazy('cliente_list')
 
+
 class ClienteUpdateView(UpdateView):
     """Vista que permite editar un cliente existente."""
     model = Cliente
@@ -142,16 +144,19 @@ class ClienteUpdateView(UpdateView):
         """Redirige al cliente que se ha actualizado."""
         return reverse('cliente_detail', kwargs={'pk': self.object.pk})
 
+
 class ClienteDeleteView(DeleteView):
     """Vista que permite eliminar un cliente."""
     model = Cliente
     template_name = 'cliente_confirm_delete.html'
     context_object_name = 'cliente'
     success_url = reverse_lazy('cliente_list')
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context = if_detail(self.request, context)
         return context
+
 
 
 # EMPLEADO
@@ -161,18 +166,20 @@ class EmpleadoListView(ListView):
     template_name = 'empleado_list.html'
     context_object_name = 'empleados'
     ordering = ['nombre']
+
     def get(self, request, *args, **kwargs):
         vengo_de = request.GET.get('vengo_de', None)
         if vengo_de == "boton_arriba":
             request.session['origen'] = request.build_absolute_uri()
         return super().get(request, *args, **kwargs)
 
+
 class EmpleadoDetailView(DetailView):
     """Vista que muestra el detalle de un empleado específico."""
     model = Empleado
     template_name = 'empleado_detail.html'
     context_object_name = 'empleado'
-    
+
     def get_context_data(self, **kwargs):
         """Recupera todos proyectos donde el empleado es responsable."""
         context = super().get_context_data(**kwargs)
@@ -182,13 +189,13 @@ class EmpleadoDetailView(DetailView):
         return context
     
     
-
 class EmpleadoCreateView(CreateView):
     """Vista que permite crear un nuevo empleado."""
     model = Empleado
     fields = '__all__'
     template_name = 'empleado_form.html'
     success_url = reverse_lazy('empleado_list')
+
     def form_valid(self, form):
         response = super().form_valid(form) 
         empleado = self.object   
@@ -212,8 +219,6 @@ class EmpleadoCreateView(CreateView):
         Saludos cordiales,  
         Deustotil S.L
         """
-
-
            
 
         # send_mail(
@@ -224,12 +229,9 @@ class EmpleadoCreateView(CreateView):
         #     fail_silently=False,
             
         # )
-        
 
         return response
         
-        
-
 
 class EmpleadoUpdateView(UpdateView):
     """Vista que permite editar un empleado existente."""
@@ -242,16 +244,19 @@ class EmpleadoUpdateView(UpdateView):
         """Redirige al empleado que se ha actualizado."""
         return reverse('empleado_detail', kwargs={'pk': self.object.pk})
 
+
 class EmpleadoDeleteView(DeleteView):
     """Vista que permite eliminar un empleado."""
     model = Empleado
     template_name = 'empleado_confirm_delete.html'
     success_url = reverse_lazy('empleado_list')
     context_object_name = 'empleado'
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context = if_detail(self.request, context)
         return context
+
 
 
 # TAREA
@@ -260,6 +265,7 @@ class TareaListView(ListView):
     model = Tarea
     template_name = 'tarea_list.html'
     context_object_name = 'tareas'
+
     def get(self, request, *args, **kwargs):
         vengo_de = request.GET.get('vengo_de', None)
         if vengo_de == "boton_arriba":
@@ -272,11 +278,13 @@ class TareaListView(ListView):
         context['fecha_actual'] = date.today()
         return context
 
+
 class TareaDetailView(DetailView):
     """Vista que muestra el detalle de una tarea específica."""
     model = Tarea
     template_name = 'tarea_detail.html'
     context_object_name = 'tarea'
+
 
 class TareaCreateView(CreateView):
     """Vista que permite crear una nueva tarea."""
@@ -284,6 +292,7 @@ class TareaCreateView(CreateView):
     fields = '__all__'
     template_name = 'tarea_form.html'
     success_url = reverse_lazy('tarea_list')
+
 
 class TareaUpdateView(UpdateView):
     """Vista que permite editar una tarea existente."""
@@ -296,15 +305,15 @@ class TareaUpdateView(UpdateView):
         """Redirige a la tarea que se ha actualizado."""
         return reverse('tarea_detail', kwargs={'pk': self.object.pk})
 
+
 class TareaDeleteView(DeleteView):
     """Vista que permite eliminar una tarea."""
     model = Tarea
     template_name = 'tarea_confirm_delete.html'
     context_object_name = "tarea"
     success_url = reverse_lazy('tarea_list')
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context = if_detail(self.request, context)
         return context
-
-    
